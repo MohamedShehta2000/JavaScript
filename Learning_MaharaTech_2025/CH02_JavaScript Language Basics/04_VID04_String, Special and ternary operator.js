eb05e049b0409a2b6ffcc0f76cb31f9112c6b098
Mo.shehta <mohamedshehta2024@gmail.com>
2025-02-26 03:22:11 +0200

    video4_string_special and ternary operators

---
+++ Learning_MaharaTech_2025/CH02_JavaScript Language Basics/04_VID04_String, Special and ternary operator.js
@@ -0,0 +1,24 @@
+// String operators (+)
+var firstname = "Ali";
+var lastname = "kareem";
+var fullname = firstname + lastname;
+console.log(fullname);
+
+// or can we write with another way :
+var firstname = "Ali",
+lastname = "kareem",
+fullname = firstname + lastname,
+console.log(fullname)
+
+// Special operators (comma operators (,) , typeof  , Ternary )
+// Ternary (condition?val1:val2)
+// vla1 if condition is true , val2 if condition is false 
+<script>
+    var temp = 120 ;
+        newvar = (temp>100) ? `red` : `blue`;
+        // the value of newvar is red
+        temp = 20 ;
+        newvar = (temp>100) ? `red` : `blue`;
+        // the value of newvar is blue
+</script>
+
