b2322f841aa22a435b2fea15eee914142e1b682e
Mo.shehta <mohamedshehta2024@gmail.com>
2025-03-01 04:01:09 +0200

    Dialogue boxes alert, prompt and confirm

---
+++ Learning_MaharaTech_2025/CH02_JavaScript Language Basics/08_VID08_Dialogue_boxes _in _JS.js
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <title>Title</title>
+    </head>
+    <body>
+         <script>
+             var x = 10;
+             alert("this is alert");
+             alert("value of x is:" + x);
+             
+            var response = prompt("enter your name","mohamed");
+             
+            var action = confirm("do want to continue?");
+         
+         </script>
+    </body>
+</html>
