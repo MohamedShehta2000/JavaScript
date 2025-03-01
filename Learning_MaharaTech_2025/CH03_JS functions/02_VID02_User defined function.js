d3c63e57304ce1e504eda7aa439c19101fc37133
Mo.shehta <mohamedshehta2024@gmail.com>
2025-03-01 14:44:21 +0200

    user defined functions

---
+++ Learning_MaharaTech_2025/CH03_JS functions/02_VID02_User defined function.js
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <title>Untitled Document</title>
+    </head>
+    <body>
+        <script>
+            Function addTwoPara(x,y) {
+                //var sum = 0;
+                sum = x + y ;
+                return x + y ;
+            }
+            var resullt = addTwoPara(5,6);
+            console.log(result);
+        </script>
+    </body>
+</html>
