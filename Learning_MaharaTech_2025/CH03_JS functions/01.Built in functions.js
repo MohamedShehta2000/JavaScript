be18039f47a4f2d47bdb2ebe3f2896e91d00d0fd
Mo.shehta <mohamedshehta2024@gmail.com>
2025-03-01 14:03:55 +0200

    change formula

---
+++ Learning_MaharaTech_2025/CH03_JS functions/01.Built in functions.js
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <title>Untitled Document</title>
+    </head>
+    <body>
+        <script>
+            var num1 = parseInt(prompt("enter first value","10"));
+            var num2 = parseInt(prompt("enter second value","11"));
+            //alert(parseInt(num1),parseInt(num2));
+            alert(num1+num2)
+        </script>
+    </body>
+</html>
