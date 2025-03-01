fb86eb3d26cf1be0c35465c884d79f4dd60698e7
Mo.shehta <mohamedshehta2024@gmail.com>
2025-03-01 02:32:31 +0200

    eedit1 in 01_varibales.js

---
+++ Learning_MaharaTech_2025/CH02_JavaScript Language Basics/01.VID01_Varibales.js
@@ -0,0 +1,15 @@
+<script>
+    console.log(x);
+    var x = 10;
+    console.log(x);
+    console.log(typeof x);
+
+    x = "Hello";
+    console.log(x);
+    console.log(typeof x);
+
+    x = false;
+    console.log(typeof x);
+
+    x = null;
+</script>
