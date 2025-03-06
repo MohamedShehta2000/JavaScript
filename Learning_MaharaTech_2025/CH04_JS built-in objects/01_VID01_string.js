4ab4a2cd287fce70c46930445a6218c4264d2832
Mo.shehta <mohamedshehta2024@gmail.com>
2025-03-06 08:38:24 +0200

    ch04 js string

---
+++ Learning_MaharaTech_2025/CH04_JS built-in objects/01_VID01_string.js
@@ -0,0 +1,14 @@
+// literal function 
+var myStr = "Welcome to JavaScript world!!";
+
+// constructor 
+var newStr = new String ("JavaScript String Object");
+
+// String ()
+var Str = String("")
+
+console.log(typeof myStr)
+console.log(typeof newStr)
+console.log(typeof Str)
+
+console.log(myStr.length);
