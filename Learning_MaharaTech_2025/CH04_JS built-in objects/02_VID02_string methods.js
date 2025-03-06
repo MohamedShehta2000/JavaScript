64dec47b0bc6a2b2089a6dec7111a9cde957f1e0
Mo.shehta <mohamedshehta2024@gmail.com>
2025-03-06 09:43:43 +0200

    some of objects in js

---
+++ Learning_MaharaTech_2025/CH04_JS built-in objects/02_VID02_string methods.js
@@ -0,0 +1,25 @@
+// literal function 
+var myStr = "Welcome to JavaScript world!!";
+
+// constructor 
+var newStr = new String ("JavaScript String Object");
+
+// String ()
+var Str = String("")
+
+console.log(typeof myStr) //string
+console.log(typeof newStr) //object
+console.log(typeof Str) //string
+
+console.log(myStr.length); // 29
+
+console.log(myStr.charAt(0)); //w
+console.log(myStr.charAt(myStr.length-1)); // !
+
+console.log(myStr.indexOf("j"));
+console.og(myStr.lastIndexOf("Sc"));
+
+console.log(myStr.substring("4,10")); //elcome
+
+console.log(myStr.split(" ")); // (5) ['welcome', 'to', 'javascript','world!!']
+console.log(myStr.split("")); // (32)
