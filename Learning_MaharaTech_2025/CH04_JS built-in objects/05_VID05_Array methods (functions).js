24aed30608c45a53768db1062b5389ebea2cd76a
Mo.shehta <mohamedshehta2024@gmail.com>
2025-03-08 14:49:09 +0200

    video 5 Array methods

--- Learning_MaharaTech_2025/CH04_JS built-in objects/05_VID05_Array methods (functions).js
+++ Learning_MaharaTech_2025/CH04_JS built-in objects/05_VID05_Array methods (functions).js
@@ -4,11 +4,13 @@
  * Array >> Methods
  */
 
-// 1. newArray.join()
-
 var newArr = [1,2,3,5,10,11,7,26];
-
+// 01. newArray.join()
 console.log(newArr.join("*_*")); //1*_*2.......*_*26
 console.log(newArr.join("*")); // 1*2*3..........*26
 console.log(newArr.join("")); // 12351011726
 console.log(typeof newArr.join("*_*"));  // string
+// 02. nweArray.push()
+console.log(newArr.push("new value"));
+// 03. newArray.pop()
+console.log(newArr.pop());
